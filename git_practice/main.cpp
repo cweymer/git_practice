c23d86db99556753d1d86a305b2b4c5abeb4cf48
cweymer <cweymer@g.ucla.edu>
2023-02-03 23:48:33 -0800

    Initial Commit

---
+++ git_practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  git_practice
+//
+//  Created by Charlotte Weymer on 2/3/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
